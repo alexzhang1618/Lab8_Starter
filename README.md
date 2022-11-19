27de3c6a907e97aac4dbd9e8bdaaca90c205130a
Alex Zhang <alexzhang1618@gmail.com>
2022-11-18 23:45:09 -0800

    Finish Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+Alex Zhang
+
+1) I would put the automated tests in a GitHub action (1) that runs whenever code is pushed so tests are run automatically and are consistently run while working on the repository.
+2) You would not use an end-to-end test to check if a function is returning the correct output because you could just directly test the function without emulating user actions.
+3) An end-to-end test would be more useful to test the messaging feature of an application because a unit test isn't able to verify if the message is received or correctly displayed.
+4) A unit test would be good to test the max messaging length because the function can be called and checked to see if it correctly caps the message limit.
+
+![](./tests1.png)
+![](./tests2.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,77 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+describe('isPhoneNumber', () => {
+    test('isPhoneNumber', () => {
+        expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+    });
+    test('isPhoneNumber', () => {
+        expect(functions.isPhoneNumber('999-111-1222')).toBe(true);
+    });
+    test('isPhoneNumber', () => {
+        expect(functions.isPhoneNumber('34567890')).toBe(false);
+    });
+    test('isPhoneNumber', () => {
+        expect(functions.isPhoneNumber('')).toBe(false);
+    });
+})
+
+describe('isEmail', () => {
+    test('isEmail', () => {
+        expect(functions.isEmail('john@email.com')).toBe(true);
+    });
+    test('isEmail', () => {
+        expect(functions.isEmail('jane@email.org')).toBe(true);
+    });
+    test('isEmail', () => {
+        expect(functions.isEmail('@email.com')).toBe(false);
+    });
+    test('isEmail', () => {
+        expect(functions.isEmail('')).toBe(false);
+    });
+});
+
+describe('isStrongPassword', () => {
+    test('isStrongPassword', () => {
+        expect(functions.isStrongPassword('asdf')).toBe(true);
+    });
+    test('isStrongPassword', () => {
+        expect(functions.isStrongPassword('asdf11331223')).toBe(true);
+    });
+    test('isStrongPassword', () => {
+        expect(functions.isStrongPassword('')).toBe(false);
+    });
+    test('isStrongPassword', () => {
+        expect(functions.isStrongPassword('1234981270359812734239084')).toBe(false);
+    });
+});
+
+describe('isDate', () => {
+    test('isDate', () => {
+        expect(functions.isDate('12/12/2022')).toBe(true);
+    });
+    test('isDate', () => {
+        expect(functions.isDate('1/1/2022')).toBe(true);
+    });
+    test('isDate', () => {
+        expect(functions.isDate('')).toBe(false);
+    });
+    test('isDate', () => {
+        expect(functions.isDate('111/22/2022')).toBe(false);
+    });
+});
+
+describe('isHexColor', () => {
+    test('isHexColor', () => {
+        expect(functions.isHexColor('000')).toBe(true);
+    });
+    test('isHexColor', () => {
+        expect(functions.isHexColor('fff000')).toBe(true);
+    });
+    test('isHexColor', () => {
+        expect(functions.isHexColor('')).toBe(false);
+    });
+    test('isHexColor', () => {
+        expect(functions.isHexColor('f')).toBe(false);
+    });
+});
